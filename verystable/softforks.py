0e7f504d9e8640cea9292a7957afe78390b5ffb1
James O'Beirne <james.obeirne@pm.me>
2023-09-13 12:07:34 -0400

    add BIP-119 (CTV) activation

--- verystable/softforks.py
+++ verystable/softforks.py
@@ -1,5 +1,8 @@
 
-from .core import script
+import types
+import struct
+
+from .core import script, messages
 
 
 def activate_bip345_vault():
@@ -11,3 +14,25 @@ def activate_bip345_vault():
 
     script.OPCODE_NAMES[script.OP_VAULT] = 'OP_VAULT'
     script.OPCODE_NAMES[script.OP_VAULT_RECOVER] = 'OP_VAULT_RECOVER'
+
+
+def activate_bip119_ctv():
+    script.OP_CHECKTEMPLATEVERIFY = script.CScriptOp(0xb3)
+    script.OPCODE_NAMES[script.OP_CHECKTEMPLATEVERIFY] = 'OP_CHECKTEMPLATEVERIFY'
+
+    messages.CTransaction.get_standard_template_hash = (
+        types.MethodType(_get_standard_template_hash, messages.CTransaction))
+
+
+def _get_standard_template_hash(self, nIn):
+    r = b""
+    r += struct.pack("<i", self.nVersion)
+    r += struct.pack("<I", self.nLockTime)
+    if any(inp.scriptSig for inp in self.vin):
+        r += messages.sha256(b"".join(messages.ser_string(inp.scriptSig) for inp in self.vin))
+    r += struct.pack("<I", len(self.vin))
+    r += messages.sha256(b"".join(struct.pack("<I", inp.nSequence) for inp in self.vin))
+    r += struct.pack("<I", len(self.vout))
+    r += messages.sha256(b"".join(out.serialize() for out in self.vout))
+    r += struct.pack("<I", nIn)
+    return messages.sha256(r)
